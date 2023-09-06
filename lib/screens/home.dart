14dc853a67b03f887a6baf6c7fbba85a0e3fbc5c
n1ved <nivedrsalini@gmail.com>
2023-09-06 11:25:32 +0530

    more fields , program wont work unless there is way to pass by referance

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _tctlTemp = TextEditingController();
+  final TextEditingController _stapmLimit = TextEditingController();
+  final TextEditingController _fastLimit = TextEditingController();
+  final TextEditingController _slowLimit = TextEditingController();
   int _tctlTempVal = 90;
+  int _stapmLimitVal = 45000;
+  int _fastLimitVal = 45000;
+  int _slowLimitVal = 45000;
 
   var sensor_tctl;
   Timer? timer;
@@ -64,15 +70,40 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Expanded(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextField(
-                      controller: _tctlTemp,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        setState(() {
-                          _tctlTempVal = int.parse(value);
-                        });
+                    ControllerInputBox(
+                      inputController: _tctlTemp,
+                      onChanged: () {
+                        _tctlTempVal = int.parse(_tctlTemp.value.toString());
                       },
+                      inputIcon: Icons.thermostat,
+                      labelText: "Temperature Limit",
+                    ),
+                    ControllerInputBox(
+                      inputController: _stapmLimit,
+                      onChanged: () {
+                        _stapmLimitVal =
+                            int.parse(_stapmLimit.value.toString());
+                      },
+                      inputIcon: Icons.computer,
+                      labelText: "CPU TDP",
+                    ),
+                    ControllerInputBox(
+                      inputController: _fastLimit,
+                      onChanged: () {
+                        _fastLimitVal = int.parse(_fastLimit.value.toString());
+                      },
+                      inputIcon: Icons.computer,
+                      labelText: "CPU FAST TDP",
+                    ),
+                    ControllerInputBox(
+                      inputController: _slowLimit,
+                      onChanged: () {
+                        _slowLimitVal = int.parse(_slowLimit.value.toString());
+                      },
+                      inputIcon: Icons.computer,
+                      labelText: "CPU SLOW TDP",
                     ),
                   ],
                 ),
@@ -84,11 +115,21 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           var shell = Shell();
-          await shell.run("ryzenadj --tctl-temp=${_tctlTempVal.toString()}");
+          await shell.run(
+              "ryzenadj --tctl-temp=${_tctlTempVal.toString()} --stapm-limit=${_stapmLimitVal.toString()} --fast-limit=${_fastLimitVal.toString()} --slow-limit=${_slowLimitVal.toString()}");
         },
-        child: Icon(Icons.start),
+        child: const Icon(Icons.start),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _tctlTemp.dispose();
+    _fastLimit.dispose();
+    _stapmLimit.dispose();
+    _slowLimit.dispose();
+    super.dispose();
   }
 }
 
@@ -116,6 +157,46 @@ class _DataTextState extends State<DataText> {
           style: Theme.of(context).textTheme.displayLarge,
         ),
       ],
+    );
+  }
+}
+
+class ControllerInputBox extends StatefulWidget {
+  ControllerInputBox(
+      {super.key,
+      required this.inputController,
+      required this.onChanged,
+      required this.inputIcon,
+      required this.labelText});
+
+  final TextEditingController inputController;
+  final Function() onChanged;
+  final IconData inputIcon;
+  final String labelText;
+
+  @override
+  State<ControllerInputBox> createState() => _ControllerInputBoxState();
+}
+
+class _ControllerInputBoxState extends State<ControllerInputBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: TextField(
+        controller: widget.inputController,
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          setState(() {
+            widget.onChanged;
+          });
+        },
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          border: const OutlineInputBorder(),
+          prefixIcon: Icon(widget.inputIcon),
+        ),
+      ),
     );
   }
 }
